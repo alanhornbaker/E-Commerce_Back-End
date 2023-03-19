65cc49e3f191f6eed6f70a757d1ea61011246ab7
Alan Hornbaker <alanhornbaker@gmail.com>
2023-03-18 20:38:16 -0500

    Created 'Find all Categories' route.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,11 @@ const { Category, Product } = require("../../models");
 router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [{ model: Product }],
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 router.get("/:id", (req, res) => {
